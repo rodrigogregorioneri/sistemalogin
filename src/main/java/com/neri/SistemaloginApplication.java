46f93430289780874d9014e31ee249608a032ad8
rodrigogregorioneri <familiamenores0>
2018-06-05 07:53:38 -0300

    Initial commit

---
+++ src/main/java/com/neri/SistemaloginApplication.java
@@ -0,0 +1,12 @@
+package com.neri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaloginApplication.class, args);
+	}
+}
---
+++ src/test/java/com/neri/SistemaloginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neri;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaloginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
